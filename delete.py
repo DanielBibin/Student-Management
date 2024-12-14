975da43518452e44608c4ab15285673dcc8acb93
Daniel Bibin <danielbibin9@gmail.com>
2024-12-14 16:00:33 +0530

    Update delete.py

--- delete.py
+++ delete.py
@@ -5,7 +5,7 @@ class Delete:
         st.header("Deletion") 
         global Deletion_Form
         Deletion_Form = st.form("Deletion Form")#creating streamlit form
-        def Deletion():  
+        def Deletion(Deletion_Form):  
             conn = sq.connect("Student_Management.db")
             cursor = conn.cursor()
             global cnt
@@ -30,6 +30,6 @@ class Delete:
             Adno = st.text_input("Enter Admission Number Of Student Record To Be Deleted:")#text input to input admission number to be deleted
             col1, col2, col3, col4, col5 = st.columns([1,1,1,1,1])#creating columns for better button placing 
             with col3:#using column 3
-                st.form_submit_button("Delete", on_click=Deletion())#form submit button
+                st.form_submit_button("Delete", on_click=lambda: Deletion(Deletion_Form))#form submit button
             
         
